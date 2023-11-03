fb964665feb99930e0dff0edcbe305ac8e195262
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-11-03 14:40:42 -0400

    Update jmdb_main.js

--- site/jmdb_main.js
+++ site/jmdb_main.js
@@ -8,6 +8,29 @@ function setup() {
   button = createButton("Fetch");
   button.mousePressed(fetchData);
   button.position(100, 300);
+
+  //movie bar
+  const Mbutton = createButton("Load Person");
+  Mbutton.position(0, 0);
+  Mbutton.mousePressed(load);
+  
+  loadPerson = createInput();
+  loadPerson.size(85);
+  loadPerson.position(100, 0);
+}
+
+function load() {
+  data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=${loadPerson.value()}&api_key=`+ TMDB_API_KEY, function(data){
+    const myMovie = new Movie(data);
+    print(data);
+    myMovie.getImage(200, 100);
+    //text(myMovie.getName(), 10, 10);
+    //text(myMovie.getAbout(), 10, 20);
+    //text(myMovie.getScore(), 10, 50);
+    //text(myMovie.getDate(), 10, 60);
+  } );
+  
+  
 }
 
   function draw() {
