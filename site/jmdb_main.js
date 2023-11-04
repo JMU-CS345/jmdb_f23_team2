a8d143a7d9c5e20ee7a9bfc28dcd3a63f6868f6e
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-11-04 01:04:08 -0400

    actor search bar

--- site/jmdb_main.js
+++ site/jmdb_main.js
@@ -10,16 +10,25 @@ function setup() {
   button.position(100, 300);
 
   //movie bar
-  const Mbutton = createButton("Load Person");
+  const Mbutton = createButton("Load Scroovie");
   Mbutton.position(0, 0);
-  Mbutton.mousePressed(load);
+  Mbutton.mousePressed(loadMovie);
   
   loadPerson = createInput();
   loadPerson.size(85);
   loadPerson.position(100, 0);
+
+  //actor bar
+  const Abutton = createButton("Load Actor");
+  Abutton.position(220, 0);
+  Abutton.mousePressed(loadActor);
+    
+    inputActor = createInput();
+    inputActor.size(85);
+    inputActor.position(300, 0);
 }
 
-function load() {
+function loadMovie() {
   data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=${loadPerson.value()}&api_key=`+ TMDB_API_KEY, function(data){
     const myMovie = new Movie(data);
     print(data);
@@ -29,12 +38,27 @@ function load() {
     //text(myMovie.getScore(), 10, 50);
     //text(myMovie.getDate(), 10, 60);
   } );
+
+  
   
   
 }
 
+function loadActor() {
+  data = loadJSON(`https://api.themoviedb.org/3/search/person?api_key=efa84f4c1b06a034b1928da13517c220&query=${inputActor.value()}`, function(data){
+    const myActor = new Actor(data);
+    print(data);
+    myActor.getImage(300, 100);
+    text(myActor.getName(), 10, 10);
+    text(myActor.getScore(), 10, 50);
+  } );
+}
+
   function draw() {
-    background(0);
+    background(128, 0, 128);
+    stroke(50);
+    fill(100);
+    ellipse(200, 12, 50, 50);
   }
 
   function fetchData() {
