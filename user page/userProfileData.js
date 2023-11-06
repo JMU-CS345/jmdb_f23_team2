18139b5fcfa48e2edb586dada0bc2ce88c7ac390
Josh Derrow <derrowjb@dukes.jmu.edu>
2023-11-06 14:44:56 -0500

    Update userProfileData.js

--- user page/userProfileData.js
+++ user page/userProfileData.js
@@ -14,7 +14,7 @@ class User {
         keyval.set(this.nextAvailableUserID, createJSONlayout(), none)
     }
 
-    update(){
+    update() {
         keyval.set(this.nextAvailableUserID, createJSONlayout(), none)
     }
 
@@ -33,7 +33,6 @@ class User {
         return userJSON
     }
 
-<<<<<<< Updated upstream:user page/userProfileData.js
     addFriend(friend) {
         this.friends.push(friend);
         this.update();
@@ -43,12 +42,8 @@ class User {
         this.friends.splice(this.friends.indexOf(friend), 1);
         this.update();
     }
-
     
-
 }
-=======
 function createAccount() {
     tempUser = new User("Josh", "Derrow", "joshb12d@gmail.com", "password");
 }
->>>>>>> Stashed changes:site/userProfileData.js
