9749fb1badcb135d5053b8f5671a7f474879dfee
Will <wgkeppel@gmail.com>
2023-11-06 13:40:27 -0500

    Updated Placements for buttons

--- site/jmdb_main.js
+++ site/jmdb_main.js
@@ -12,32 +12,32 @@ function setup() {
 
   //movie bar
   const Mbutton = createButton("Load Scroovie");
-  Mbutton.position(0, 0);
+  Mbutton.position(windowWidth / 2 - 250, windowHeight / 2 - 200);
   Mbutton.mousePressed(loadMovie);
   
   loadPerson = createInput();
   loadPerson.size(85);
-  loadPerson.position(100, 0);
+  loadPerson.position(windowWidth / 2 - 145, windowHeight / 2 - 200);
 
   //actor bar
   const Abutton = createButton("Load Actor");
-  Abutton.position(220, 0);
+  Abutton.position(windowWidth / 2, windowHeight / 2 - 200);
   Abutton.mousePressed(loadActor);
     
     inputActor = createInput();
     inputActor.size(85);
-    inputActor.position(300, 0);
+    inputActor.position(windowWidth / 2 + 85, windowHeight / 2 - 200);
 }
 
 function loadMovie() {
   data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=${loadPerson.value()}&api_key=`+ TMDB_API_KEY, function(data){
     const myMovie = new Movie(data);
     print(data);
-    myMovie.getImage(0, 100);
-    //text(myMovie.getName(), 10, 10);
-    //text(myMovie.getAbout(), 10, 20);
-    //text(myMovie.getScore(), 10, 50);
-    //text(myMovie.getDate(), 10, 60);
+    myMovie.getImage(windowWidth / 2 - 230, windowHeight / 2 - 150);
+    text(myMovie.getName(), 10, 10);
+    text(myMovie.getAbout(), 10, 20);
+    text(myMovie.getScore(), 10, 50);
+    text(myMovie.getDate(), 10, 60);
   } );
 
   
@@ -49,9 +49,9 @@ function loadActor() {
   data = loadJSON(`https://api.themoviedb.org/3/search/person?api_key=efa84f4c1b06a034b1928da13517c220&query=${inputActor.value()}`, function(data){
     const myActor = new Actor(data);
     print(data);
-    myActor.getImage(300, 100);
-    text(myActor.getName(), 10, 10);
-    text(myActor.getScore(), 10, 50);
+    myActor.getImage(windowWidth / 2 + 30, windowHeight / 2 - 150);
+    text(myActor.getName(), windowWidth / 2, windowHeight / 2 - 100);
+    text(myActor.getScore(), windowWidth / 2 + 85, windowHeight / 2 - 80);
   } );
 }
 
