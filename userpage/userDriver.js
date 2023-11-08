5776f7d5a83e31071479c600a6aa013171cc4178
Will <wgkeppel@gmail.com>
2023-11-08 14:31:57 -0500

    Update userDriver.js

--- userpage/userDriver.js
+++ userpage/userDriver.js
@@ -1,14 +1,6 @@
 function setup (){
     user = localStorage.getItem(username);
     userProfileData.createAccount(user);
-    
-}
-
-function draw (){
-    
-}
-
-function setup() {
     createCanvas(0, 0);
 
     //Create Temp User
@@ -18,5 +10,9 @@ function setup() {
     let title = createP(temp.getFirstName() + "'s Profile");
     title.position(windowWidth / 2 - 90, 20);
     title.style("color", "#CBB677");
-    title.style("font-size", "36px")
+    title.style("font-size", "36px");
+}
+
+function draw (){
+    
 }
