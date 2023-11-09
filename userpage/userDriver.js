5febf03f586f48537e74ac666bbfd57507e57298
Will <wgkeppel@gmail.com>
2023-11-09 14:57:23 -0500

    Update userDriver.js

--- userpage/userDriver.js
+++ userpage/userDriver.js
@@ -4,12 +4,12 @@ function setup (){
     createCanvas(0, 0);
 
     //Create Temp User
-    temp = userProfileData.createAccount("Josh", "Derrow", "joshb12d@gmail.com", "password");
+    temp = new User("Josh", "Derrow", "joshb12d@gmail.com", "password");
 
     //title
     let title = createP(temp.getFirstName() + "'s Profile");
     title.position(windowWidth / 2 - 90, 20);
-    title.style("color", "#CBB677");
+    title.style("color", "#f00000");
     title.style("font-size", "36px");
 }
 
