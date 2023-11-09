d4c91c9be4f35106bdeb63067aba99996c733ae9
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-11-09 12:34:54 -0500

    Update createProfile.js

--- create profile/createProfile.js
+++ create profile/createProfile.js
@@ -2,36 +2,32 @@
 let keyval = new Keyval(KEYVAL_API_KEY);
 
 function setup() {
-  /*let input = createInput();
-  /input.position(100, 275);
-  /button = createButton("Fetch");
-  /button.mousePressed(fetchData);
-  /button.position(100, 300);
-  */
+
 
 
   // textboxes
   saveButton = createButton("Create Account");
+  saveButton.position(windowWidth / 2, 700);
   firstNameBox = createInput("");
-  saveComment.attribute("placeholder", "First Name");
-  firstNameBox.position(100, 100);
+  firstNameBox.attribute("placeholder", "First Name");
+  firstNameBox.position(windowWidth / 2, 300);
   lastNameBox = createInput("");
-  saveComment.attribute("placeholder", "Last Name");
-  lastNameBox.position(100, 200);
+  lastNameBox.attribute("placeholder", "Last Name");
+  lastNameBox.position(windowWidth / 2, 400);
   emailbox = createInput("");
-  saveComment.attribute("placeholder", "Email");
-  emailbox.position(100, 300);
+  emailbox.attribute("placeholder", "Email");
+  emailbox.position(windowWidth / 2, 500);
   passwordBox = createInput("");
-  saveComment.attribute("placeholder", "Password");
-  passwordBox.position(100, 350);
-}
+  passwordBox.attribute("placeholder", "Password");
+  passwordBox.position(windowWidth / 2, 600);
 
-  //title
+
+  
   let title = createP("Scroovie");
   title.position(windowWidth / 2 - 90, 20);
   title.style("color", "#CBB677");
   title.style("font-size", "36px")
-
+}
 
   function draw() {
     background(69, 0, 132);
