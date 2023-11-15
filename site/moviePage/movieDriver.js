7181ce5b13c5c1c4571db6d28c7edbfaf8a327b5
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-11-15 14:30:49 -0500

    Update movieDriver.js

--- site/moviePage/movieDriver.js
+++ site/moviePage/movieDriver.js
@@ -1,24 +1,41 @@
 let movie;
+let myPos = 50; //adjust height of all text
 
 function setup() {
+    //createCanvas(windowWidth, windowHeight);
+    Title = createP();
     const watchlist = createButton("Add To Watch List");
+    watchlist.position(0,20);
     const seen = createButton("Add To Seen");
+    seen.position(0,40);
     const favorites = createButton("Add To Favorite");
+    favorites.position(0,60);
     watchlist.mousePressed(addToWatchList);
     seen.mousePressed(addToSeen);
     favorites.mousePressed(addToFavorites);
     data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=$inception&api_key=`+ TMDB_API_KEY, function(data){
     const myMovie = new Movie(data);
-    print(data);
-    myMovie.getBigImage(windowWidth, windowHeight / 2 - 150);
-    myMovie.getBackground();
-    text(myMovie.getName(), 10, 10);
-    text(myMovie.getAbout(), 10, 20);
-    text(myMovie.getScore(), 10, 50);
-    text(myMovie.getDate(), 10, 60);
+
+    myMovie.getBigImage(windowWidth, windowHeight);
+
+    Title = createP("Title: " + myMovie.getName());
+    Title.position(windowWidth / 2, myPos);
+    Title.style("font-size", "46px");
+
+    Release = createP("Date: " + myMovie.getDate());
+    Release.position(windowWidth / 2, myPos + 50);
+    Release.style("font-size", "46px");
+
+    AboutTitle = createP("About: ")
+    AboutTitle.position(windowWidth / 2, myPos + 100);
+    AboutTitle.style("font-size", "46px");
+
+    About = createP( myMovie.getAbout());
+    About.position(windowWidth / 2, myPos + 170);
+    About.style('max-width', '400px');
+    About.style("font-size", "30px");
   } );
-    //movie.getBigImage(10,10);
-    //movie.getImage(50,50);
+
 }
 
 function addToWatchList(username) {
