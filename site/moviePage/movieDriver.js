3baafe54a549e6338e8779ea6b6ea52f19a393f9
LukeMarsshall <146890169+LukeMarsshall@users.noreply.github.com>
2023-11-15 14:05:27 -0500

    organized site folder

--- site/moviePage/movieDriver.js
+++ site/moviePage/movieDriver.js
@@ -1,4 +1,4 @@
-setup() {
+function setup() {
     const watchlist = createButton("Add To Watch List");
     const seen = createButton("Add To Seen");
     const favorites = createButton("Add To Favorite");
