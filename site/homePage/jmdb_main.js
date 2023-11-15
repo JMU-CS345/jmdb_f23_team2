ab7fcfc079837c1fbde088b9662b6a3a47b334db
Josh Derrow <derrowjb@dukes.jmu.edu>
2023-11-15 14:45:07 -0500

    Add button functionality

--- site/homePage/jmdb_main.js
+++ site/homePage/jmdb_main.js
@@ -14,7 +14,7 @@ function setup() {
   const Mbutton = createButton("Load Scroovie");
   Mbutton.position(windowWidth / 2 - 250, windowHeight / 2 - 200);
   Mbutton.mousePressed(loadMovie);
-  
+
   loadPerson = createInput();
   loadPerson.size(85);
   loadPerson.position(windowWidth / 2 - 145, windowHeight / 2 - 200);
@@ -23,20 +23,25 @@ function setup() {
   const Abutton = createButton("Load Actor");
   Abutton.position(windowWidth / 2, windowHeight / 2 - 200);
   Abutton.mousePressed(loadActor);
-    
-    inputActor = createInput();
-    inputActor.size(85);
-    inputActor.position(windowWidth / 2 + 85, windowHeight / 2 - 200);
+
+  inputActor = createInput();
+  inputActor.size(85);
+  inputActor.position(windowWidth / 2 + 85, windowHeight / 2 - 200);
 
   //title
   let title = createP("Scroovie");
   title.position(windowWidth / 2 - 90, 20);
   title.style("color", "#CBB677");
   title.style("font-size", "36px")
+
+  //test button
+  let userPageButton = createButton("To User Page");
+  userPageButton.position(10, 10);
+  userPageButton.mousePressed(goToUserPage);
 }
 
 function loadMovie() {
-  data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=${loadPerson.value()}&api_key=`+ TMDB_API_KEY, function(data){
+  data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=${loadPerson.value()}&api_key=` + TMDB_API_KEY, function (data) {
     const myMovie = new Movie(data);
     print(data);
     myMovie.getImage(windowWidth / 2 - 230, windowHeight / 2 - 150);
@@ -44,28 +49,28 @@ function loadMovie() {
     text(myMovie.getAbout(), 10, 20);
     text(myMovie.getScore(), 10, 50);
     text(myMovie.getDate(), 10, 60);
-  } );
+  });
 }
 
 function loadActor() {
-  data = loadJSON(`https://api.themoviedb.org/3/search/person?api_key=efa84f4c1b06a034b1928da13517c220&query=${inputActor.value()}`, function(data){
+  data = loadJSON(`https://api.themoviedb.org/3/search/person?api_key=efa84f4c1b06a034b1928da13517c220&query=${inputActor.value()}`, function (data) {
     const myActor = new Actor(data);
     print(data);
     myActor.getImage(windowWidth / 2 + 30, windowHeight / 2 - 150);
     text(myActor.getName(), windowWidth / 2, windowHeight / 2 - 100);
     text(myActor.getScore(), windowWidth / 2 + 85, windowHeight / 2 - 80);
-  } );
+  });
 }
 
-  function draw() {
-    background(255);
-    background(69, 0, 132);
-  }
+function draw() {
+  background(255);
+  background(69, 0, 132);
+}
 
-  function fetchData() {
-    keyval.set("test", input.value(), function (data) {
-      keyval.get("test", function (data) {
-        console.log(data);
-      });
+function fetchData() {
+  keyval.set("test", input.value(), function (data) {
+    keyval.get("test", function (data) {
+      console.log(data);
     });
-  }
+  });
+}
