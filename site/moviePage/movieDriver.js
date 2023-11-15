dedae2bafbf89c6f96339981f2c939e5d0fad328
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-11-15 10:17:55 -0500

    Update movieDriver.js

--- site/moviePage/movieDriver.js
+++ site/moviePage/movieDriver.js
@@ -7,7 +7,18 @@ function setup() {
     watchlist.mousePressed(addToWatchList);
     seen.mousePressed(addToSeen);
     favorites.mousePressed(addToFavorites);
-    movie.getImage(10,10);
+    data = loadJSON(`https://api.themoviedb.org/3/search/movie?query=$inception&api_key=`+ TMDB_API_KEY, function(data){
+    const myMovie = new Movie(data);
+    print(data);
+    myMovie.getBigImage(windowWidth, windowHeight / 2 - 150);
+    myMovie.getBackground();
+    text(myMovie.getName(), 10, 10);
+    text(myMovie.getAbout(), 10, 20);
+    text(myMovie.getScore(), 10, 50);
+    text(myMovie.getDate(), 10, 60);
+  } );
+    //movie.getBigImage(10,10);
+    //movie.getImage(50,50);
 }
 
 function addToWatchList(username) {
