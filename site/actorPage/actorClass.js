b5fa6a3b49eb52698bc921873481fa1c00574cc0
Josh Derrow <derrowjb@dukes.jmu.edu>
2023-12-08 14:40:57 -0500

    No image available sizing

--- site/actorPage/actorClass.js
+++ site/actorPage/actorClass.js
@@ -17,7 +17,12 @@ class Actor {
 
   getImage(x, y) {
     let img = createElement("img");
-    img.attribute("src", picURL + this.data.profile_path);
+    if (this.data.profile_path != null) {
+      img.attribute("src", picURL + this.data.profile_path);
+    } else {
+      img.attribute("src", "../Black_image.jpg");
+      img.style("height", "165px");
+    }
     img.position(x, y);
     img.style("width", "110px");
   }
