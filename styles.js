5c44d45f90f8437e62c4b75656b9c7782133ebda
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2023-12-08 13:21:18 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,6 +1,7 @@
 const backgroundColor="#CBB677";
 const textColor="#CBB677";
-const Font="Burbank Big Condensed";
+
+const Font= 'newFont';
 const errorColor= "#FF0000";
 const userColor= "#450084";
 const textFont="";
